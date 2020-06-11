7b8779f746ac53a5c69a8061f2a0bdb709ec48d5
Peter Hansen <phansen@brainshark.com>
2020-06-10 23:43:54 -0400

    initial commit

---
+++ Sieve/Sieve/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sieve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long upperBound = 121;
+            bool[] content = new bool[upperBound];
+
+            for (int i = 2; i < upperBound; i++)
+            {
+                content[i] = true;
+            }
+
+            for (int k = 2; k < upperBound; k++)
+            {
+                if (content[k])
+                {
+                    for (int i = 2; (i * k) < upperBound; i++)
+                    {
+                        content[i * k] = false;
+                    }
+                }
+            }
+
+            for (int i = 2; i < content.Length; i++)
+            {
+                Console.WriteLine(i + " IsPrime: " + content[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
